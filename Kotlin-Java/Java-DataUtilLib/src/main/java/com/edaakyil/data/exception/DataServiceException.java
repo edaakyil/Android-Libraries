7d9d2b26b430964b34094cfa7983d831685cd3be
edaakyil <edaakyil@gmail.com>
2025-02-26 12:01:05 +0300

    Update

---
+++ Kotlin-Java/Java-DataUtilLib/src/main/java/com/edaakyil/data/exception/DataServiceException.java
@@ -0,0 +1,24 @@
+package com.edaakyil.android.basicviews.data.service.exception;
+
+import androidx.annotation.Nullable;
+
+public class DataServiceException extends RuntimeException {
+    public DataServiceException(String message)
+    {
+        super(message);
+    }
+
+    public DataServiceException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    @Nullable
+    @Override
+    public String getMessage()
+    {
+        var cause = getCause();
+
+        return String.format("Message: %s%s", super.getMessage(), cause != null ? ", Cause Message: " + cause.getMessage() : "");
+    }
+}
